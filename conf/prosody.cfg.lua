6f00384ba098bce813158b4dc5e7c7fc45db693b
trendless <trendless@trendless.ca>
2026-03-11 11:05:11 -0600

    Update prosody.cfg.lua

--- conf/prosody.cfg.lua
+++ conf/prosody.cfg.lua
@@ -77,7 +77,6 @@ Component "conference.__DOMAIN__" "muc"
     modules_enabled = {
         "muc_meeting_id";
         "muc_domain_mapper";
-        "polls";
         --"token_verification";
         "muc_rate_limit";
         "lobby_autostart";
@@ -140,7 +139,6 @@ Component "lobby.__DOMAIN__" "muc"
     muc_room_default_public_jids = true
     modules_enabled = {
         "muc_rate_limit";
-        "polls";
     }
 
 Component "endconference.__DOMAIN__" "end_conference"
